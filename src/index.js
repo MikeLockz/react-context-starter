4bdcadbd6584fe631af2b08e3fd6d7f6a94fcec4
Mike Lockwitz <mlockwitz@gmail.com>
2019-03-31 22:22:21 -0600

    Moving user state and methods into UserProvider class. Refactoring Root component to stateless functional.

--- src/index.js
+++ src/index.js
@@ -2,35 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import LoginPage from "./LoginPage";
 import MainPage from "./MainPage";
-import UserContext from "./UserContext";
+import { UserProvider, UserConsumer } from "./UserContext";
 import "./index.css";
 
-class Root extends React.Component {
-  state = {
-    currentUser: null
-  };
-
-  handleLogin = user => {
-    this.setState({ currentUser: user });
-  };
-
-  handleLogout = () => {
-    this.setState({ currentUser: null });
-  };
-
-  render() {
-    return (
-      <UserContext.Provider
-        value={{
-          user: this.state.currentUser,
-          onLogin: this.handleLogin,
-          onLogout: this.handleLogout
-        }}
-      >
-        {this.state.currentUser ? <MainPage /> : <LoginPage />}
-      </UserContext.Provider>
-    );
-  }
+function Root() {
+  return (
+    <UserProvider>
+      <UserConsumer>
+        {({ user }) => (user ? <MainPage /> : <LoginPage />)}
+      </UserConsumer>
+    </UserProvider>
+  );
 }
 
 ReactDOM.render(<Root />, document.querySelector("#root"));
